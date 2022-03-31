618305ff04b25a4eab689e8ac136b15a0781de5c
tkcannon <taylorkcannon@gmail.com>
2022-03-31 11:19:27 -0600

    fixed https link in app.js / new build

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import Navbar from './components/Navbar';
 
 //
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3001/graphql',
+  uri: '/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
